440c2f1cc1115e275f2361b7de65cb9e4fed9fd5
Katkov1969 <126957788+Katkov1969@users.noreply.github.com>
2023-06-01 18:56:03 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5870107599:AAHQajTte25WJcG7w_OMMSPd3Gr-QMBBh6s'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'йена': 'JPY',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+}
